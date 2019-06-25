2558c616d8c7292c4f2ac935786a9030e28d5248
Jimmy Yeung <jimmy.yeung@goxip.com>
2019-06-25 13:18:37 +0800

    Project Scaffolding

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Ho Ting Jimmy Yeung on 6/25/19.
+//  Copyright © 2019 Jimmy Yeung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
